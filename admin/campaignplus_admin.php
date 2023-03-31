0ed0aeac74e018ddcbcfdf51140a506f5689ef44
secureweb16 <secureweb16@gmail.com>
2023-03-30 23:28:50 -0700

    Initial Commit

---
+++ admin/campaignplus_admin.php
@@ -0,0 +1,229 @@
+<?php
+
+
+
+/**
+
+ * The admin-specific functionality of the plugin.
+
+ *
+
+ * @link       http://example.com
+
+ * @since      1.0.0
+
+ *
+
+ * @package    Plugin_Name
+
+ * @subpackage Plugin_Name/admin
+
+ */
+
+
+
+/**
+
+ * The admin-specific functionality of the plugin.
+
+ *
+
+ * Defines the plugin name, version, and two examples hooks for how to
+
+ * enqueue the admin-specific stylesheet and JavaScript.
+
+ *
+
+ * @package    Plugin_Name
+
+ * @subpackage Plugin_Name/admin
+
+ * @author     Your Name <email@example.com>
+
+ */
+
+class Plugin_Name_Admin {
+
+
+
+	/**
+
+	 * The ID of this plugin.
+
+	 *
+
+	 * @since    1.0.0
+
+	 * @access   private
+
+	 * @var      string    $plugin_name    The ID of this plugin.
+
+	 */
+
+	private $plugin_name;
+
+
+
+	/**
+
+	 * The version of this plugin.
+
+	 *
+
+	 * @since    1.0.0
+
+	 * @access   private
+
+	 * @var      string    $version    The current version of this plugin.
+
+	 */
+
+	private $version;
+
+
+
+	/**
+
+	 * Initialize the class and set its properties.
+
+	 *
+
+	 * @since    1.0.0
+
+	 * @param      string    $plugin_name       The name of this plugin.
+
+	 * @param      string    $version    The version of this plugin.
+
+	 */
+
+	public function __construct( $plugin_name, $version ) {
+
+
+
+		$this->plugin_name = $plugin_name;
+
+		$this->version = $version;
+
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+
+
+	}
+
+	public function admin_menu(){
+	
+		add_menu_page( "Dashboard", "Armaturenbrett","manage_options","campaign_plus_menus", array( $this, 'CampaignPlus_Dashboard' ), "dashicons-businessperson", "6");
+		add_submenu_page( "campaign_plus_menus","Kontakte", "Kontakte", "manage_options", "campaign_plus_contact", array( $this, 'CampaignPlus_ContactSync' ));
+		add_submenu_page( "campaign_plus_menus", "produkte_bestellungen", "Produkte & Bestellungen", "manage_options", "campaign_plus_products_orders", array( $this, 'CampaignPlus_ProductsOrderSync' ));
+	}
+
+
+	public function CampaignPlus_Dashboard(){
+		echo "Dashboard called";
+
+	}
+
+	public function CampaignPlus_ContactSync(){
+		echo "function_1 called";
+
+	}
+
+	public function CampaignPlus_ProductsOrderSync(){
+		echo "function_3 called";
+	}
+
+
+
+	/**
+
+	 * Register the stylesheets for the admin area.
+
+	 *
+
+	 * @since    1.0.0
+
+	 */
+
+	public function enqueue_styles() {
+
+
+
+		/**
+
+		 * This function is provided for demonstration purposes only.
+
+		 *
+
+		 * An instance of this class should be passed to the run() function
+
+		 * defined in Plugin_Name_Loader as all of the hooks are defined
+
+		 * in that particular class.
+
+		 *
+
+		 * The Plugin_Name_Loader will then create the relationship
+
+		 * between the defined hooks and the functions defined in this
+
+		 * class.
+
+		 */
+
+
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/campaignplus_admin.css', array(), $this->version, 'all' );
+
+
+
+	}
+
+
+
+	/**
+
+	 * Register the JavaScript for the admin area.
+
+	 *
+
+	 * @since    1.0.0
+
+	 */
+
+	public function enqueue_scripts() {
+
+
+
+		/**
+
+		 * This function is provided for demonstration purposes only.
+
+		 *
+
+		 * An instance of this class should be passed to the run() function
+
+		 * defined in Plugin_Name_Loader as all of the hooks are defined
+
+		 * in that particular class.
+
+		 *
+
+		 * The Plugin_Name_Loader will then create the relationship
+
+		 * between the defined hooks and the functions defined in this
+
+		 * class.
+
+		 */
+
+
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/campaignplus_admin.js', array( 'jquery' ), $this->version, false );
+
+
+
+	}
+
+
+
+}
+
